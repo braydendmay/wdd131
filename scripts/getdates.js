42365bb990c75776637eca079bd797220e42f148
Brayden May <braydendmay@gmail.com>
2025-03-08 15:56:37 -0700

    mmm

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const lastModified = document.getElementById('lastModified');
+    const currentyear = document.getElementById('currentyear');
+    const currentDate = new Date();
+    lastModified.textContent = currentDate.toLocaleDateString();
+    currentyear.textContent = currentDate.toLocaleDateString();
+});
